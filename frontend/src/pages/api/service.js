07e0b5fcc5680094052cd8b1286910664e2d946f
khafidf <khfd027@gmail.com>
2023-08-07 02:30:20 +0700

    first commit

---
+++ frontend/src/pages/api/service.js
@@ -0,0 +1,27 @@
+import api from ".";
+
+const ENDPOINT = {
+	ACCOUNT: "/accounts",
+};
+
+const getAllAccounts = async () => {
+	try {
+		const accounts = await api.get(ENDPOINT.ACCOUNT);
+		return accounts;
+	} catch (err) {
+		throw Error(err);
+	}
+};
+
+const getSelectedAccount = async (slug) => {
+	try {
+		const selectedAccount = await api.get(
+			`${ENDPOINT.ACCOUNT}?filter[slug][$eqi]=${slug}&populate=*`
+		);
+		return selectedAccount;
+	} catch (err) {
+		throw Error(err);
+	}
+};
+
+export { getAllAccounts, getSelectedAccount };
